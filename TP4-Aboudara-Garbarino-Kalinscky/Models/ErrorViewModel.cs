552eb56f3f8ec4ad0ed2214780b9ce89950dfc25
MAXIMILIANO GARBARINO <47433817@ort>
2023-05-04 15:32:16 -0300

    Subiendo MVC

---
+++ TP4-Aboudara-Garbarino-Kalinscky/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP4_Aboudara_Garbarino_Kalinscky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
